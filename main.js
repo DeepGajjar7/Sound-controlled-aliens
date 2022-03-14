72650a4adef0f6b5d55fba0edefab9fe81f9112d
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-03-14 10:30:34 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 //https://teachablemachine.withgoogle.com/models/THE0KuYGb/
+function startdancing(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/THE0KuYGb/model.json',modelloaded);
+}
+
+function modelloaded(){
+    classifier.classify(gotresult);
+}
